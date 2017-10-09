d66ff0e835f07aabebb06e5c228b6269ce032569
Effluxation <patrick.dorosz@gmail.com>
2017-10-09 10:45:49 -0400

    fix: Fix bugs from feedback of previous submission

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -53,23 +53,23 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
     describe('The menu', function () {
         // Fuction that returns state of menu
         function menuHidden() {
             return $('body').hasClass('menu-hidden');
         }
 
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
         it('element is hidden by default', function () {
-            expect(menuHidden()).toBe(true);
+            expect(menuHidden()).toBeTruthy();
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* Write a test that ensures the menu changes
         * visibility when the menu icon is clicked. This test
         * should have two expectations: does the menu display when
         * clicked and does it hide when clicked again.
@@ -77,18 +77,18 @@ $(function() {
         it('changes visibility when the menu icon is clicked', function () {
             // First click to open menu
             $('.menu-icon-link').trigger('click');
-            expect(menuHidden()).toBe(false);
+            expect(menuHidden()).toBeFalsy();
 
             // Second click to hide menu
             $('.menu-icon-link').trigger('click');
-            expect(menuHidden()).toBe(true);
+            expect(menuHidden()).toBeTruthy();
 
         });
     });
 
-    // /* TODO: Write a new test suite named "Initial Entries" */
+    // /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -97,43 +97,41 @@ $(function() {
 
         beforeEach(function (done) {
             //Run loadfeed with callback
-            loadFeed(0, function () {
-                done();
-            });
+            loadFeed(0, done);
         });
 
-        it('consists of at least 1 .entry element in .feed container when loadFeed called', function (done) {
+        it('consists of at least 1 .entry element in .feed container when loadFeed called', function () {
             let feedEntries = $('.feed').find('.entry').length;
             expect(feedEntries).toBeGreaterThan(0);
-            done();
         });
 
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
         let firstFeedEntries;
         let secondFeedEntries;
-
         beforeEach(function (done) {
             // Run first loadfeed with callback
             loadFeed(0, function () {
-                firstFeedEntries = $('.feed').find('.entry');
-                // Run second loadfeed with callback
-                loadFeed(0,function () {
-                    secondFeedEntries = $('.feed').find('.entry');
+                firstFeedEntries = $('.feed').find('.entry > h2').text();
+
+                // Run second loadfeed call with a random but
+                // different feed from previous cal
+                let randomFeed = Math.floor(Math.random() * 3) + 1;
+                loadFeed(randomFeed,function () {
+                    secondFeedEntries = $('.feed').find('.entry > h2').text();
                     done();
                 });
             });
         });
 
-        it('causes content to change', function (done) {
+        it('causes content to change', function () {
             expect(firstFeedEntries).not.toEqual(secondFeedEntries);
-            done();
         });
     });
 }());
